4793e8b38939b3c5d580c1c589d6e07d9bbe3358
KerberX <DaIvan@gmail.com>
2018-10-07 02:02:19 +0300

    TStringView type alias added

--- 7zpp/SevenString.h
+++ 7zpp/SevenString.h
@@ -6,8 +6,10 @@ namespace SevenZip
 {
 	#ifdef _UNICODE
 	typedef std::wstring TString;
+	typedef std::wstring_view TStringView;
 	#else
 	typedef std::string TString;
+	typedef std::string_view TStringView;
 	#endif
 	typedef TString::value_type TCharType;
 }
